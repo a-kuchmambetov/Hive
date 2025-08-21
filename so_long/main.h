8503602717cf93af315eccabe40a0f13ff5e4683
akuchmam <akuchmam@student.hive.fi>
2025-08-21 17:54:27 +0300

    Minor changes to texteures of wall and empty space. 21.08.2025

--- so_long/main.h
+++ so_long/main.h
@@ -13,51 +13,52 @@ enum e_TEX_ID
 	TEX_WALL,
 	TEX_COLLECTIBLE,
 	TEX_EXIT,
-    TEX_PLAYER_IDLE,
-    TEX_PLAYER_WALK_LEFT,
-    TEX_PLAYER_WALK_RIGHT,
-    TEX_PLAYER_WALK_UP,
-    TEX_PLAYER_WALK_DOWN,
-    TEX_COUNT
+	TEX_PLAYER_IDLE,
+	TEX_PLAYER_WALK_LEFT,
+	TEX_PLAYER_WALK_RIGHT,
+	TEX_PLAYER_WALK_UP,
+	TEX_PLAYER_WALK_DOWN,
+	TEX_COUNT
 };
 
 typedef struct s_img
 {
-    void *img;
-    char *a;
-    int bpp;
-    int ll;
-    int endian;
-    int w;
-    int h;
+	void	*img;
+	char	*a;
+	int		bpp;
+	int		ll;
+	int		endian;
+	int		w;
+	int		h;
 } t_img;
 
 typedef struct s_game
 {
-    void *mlx;
-    void *win;
-    t_img textures[TEX_COUNT];
-    int p_pos_col;
-    int p_pos_row;
-    t_img frame;
+	void	*mlx;
+	void	*win;
+	t_img	textures[TEX_COUNT];
+	int		p_pos_col;
+	int		p_pos_row;
+	t_img	frame;
 } t_game;
 
 typedef struct s_map
 {
-    char **data;
-    int rows;
-    int cols;
+	char	**data;
+	int		rows;
+	int		cols;
 } t_map;
 
 typedef struct s_level
 {
-    int num_of_collectibles;
-    int collectible_count;
-    int player_start[2];
-    int exit[2];
+	int		num_of_collectibles;
+	int		collectible_count;
+	int		player_start[2];
+	int		exit[2];
 } t_level;
 
 // Function prototypes
+//
 
 unsigned int getp(t_img *im, int x, int y);
 void blit_copy(t_img *dst, t_img *src, int dx, int dy);
